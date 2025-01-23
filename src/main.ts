abc25ac5e552c64610db118a2467c2546ea3466c
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-01-23 12:13:58 +0100

    cors config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with environment variables
+  const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
