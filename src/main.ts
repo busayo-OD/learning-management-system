471622219b22d431135ac5e0b32c1f9ce5d3f646
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-01-24 15:05:52 +0100

    swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,29 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: 'Content-Type, Accept',
   });
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('LMS')
+    .setDescription('LMS')
+    .setVersion('1.0')
+    .addServer('https://nexel-lms.onrender.com/')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
