c88d49df727b4e360bb3fa1ca1fec43489096179
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-02-12 15:05:16 +0100

    update subject dto updated

--- src/subject/dto/update-subject.dto.ts
+++ src/subject/dto/update-subject.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdateSubjectDto {
+
+  @ApiProperty()
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   subjectCode?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   subjectName?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   description?: string;
